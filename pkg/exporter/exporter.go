d505d39a69f0c4833b41c1b0fe8a4375794c4c45
Kenji Shimizu <jrywm121@gmail.com>
2021-08-31 22:57:35 +0900

    Implements describe method

--- pkg/exporter/exporter.go
+++ pkg/exporter/exporter.go
@@ -1,10 +1,12 @@
 package exporter
 
 import (
+	"context"
 	"sync"
 
 	"github.com/44smkn/toggl_exporter/pkg/model"
 	"github.com/go-kit/kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
@@ -32,6 +34,13 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 	e.mutex.Lock()
 	defer e.mutex.Unlock()
 
-	// call
-	ch <- prometheus.MustNewConstMetric(projectDuration, prometheus.CounterValue, 0)
+	ctx := context.Background()
+	pds, err := e.GetProjectDurations(ctx)
+	if err != nil {
+		level.Error(e.Logger).Log("msg", "failed to get project durations: %v", err)
+		return
+	}
+	for _, pd := range pds {
+		ch <- prometheus.MustNewConstMetric(projectDuration, prometheus.CounterValue, pd.Duration.Seconds(), pd.ProjectName)
+	}
 }
